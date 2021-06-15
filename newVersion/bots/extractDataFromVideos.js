40ae06401c868ba31f82b8001a746813fccc574e
caio <kayoroxa@gmai.com>
2021-06-15 00:33:21 -0300

    extract data initial works

--- newVersion/bots/extractDataFromVideos.js
+++ newVersion/bots/extractDataFromVideos.js
@@ -1,43 +1,82 @@
-async function extractDataFromVideos({ addData, page }) {
+async function extractDataFromVideos({ addData, page, maxPostGet }) {
   console.log(`[BOT EXTRACT_DATA] Opening instagram account in browser...`)
   console.log(`[BOT EXTRACT_DATA] Page: ${page}`)
 
   await page.exposeFunction('getPostsID', getPostsID)
   await page.exposeFunction('getLinkForDownload', getLinkForDownload)
 
-  const dataFromVideos = await page.evaluate(async () => {
-    const exports = []
-    const postsID = await getPostsID()
-    for (let index in postsID) {
-      const postID = postsID[index]
-      const videoUrl = await getLinkForDownload(postID)
-
-      exports.push({ postID, videoUrl })
-    }
-    console.log(`[BOT EXTRACT_DATA] ${JSON.stringify(exports)}`)
-    return exports
-  })
-
-  async function getPostsID() {
-    console.log('ESTOU DENTRO DO GET_POST_ID')
+  async function getPostsID(maxPostGet, elements) {
     console.log(`[BOT EXTRACT_DATA] Getting videos IDs...`)
-    return ['Jsd1i1243'] //, 'vfi84_f3', 'sdi12f43']
+    const _postsID = []
+
+    for (let index in elements.slice(0, maxPostGet || elements.length)) {
+      const formatado = elements[index]
+        .getAttribute('href')
+        .replace('/p/', '')
+        .replace('/', '')
+      _postsID.push(formatado)
+    }
+    return _postsID // ['vfi84_f3', 'sdi12f43']
   }
 
   async function getLinkForDownload(postID) {
     const apiURL = await createApiUrl(postID)
 
     async function createApiUrl(postID) {
-      console.log(`[BOT EXTRACT_DATA] creating api url from: ${postID}...`)
-      return 'api_' + postID
-    }
+      const query = 'eaffee8f3c9c089c9904a5915a898814'
 
+      const urlResult = `https://www.instagram.com/graphql/query/?query_hash=${query}&variables=${encodeURIComponent(
+        JSON.stringify({ shortcode: postID })
+      )}`
+
+      console.log(`[BOT EXTRACT_DATA] creating api url from: ${postID}...`)
+      return urlResult
+    }
     console.log(
       `[BOT EXTRACT_DATA] getting VIDEO_URL from api url: ${apiURL}...`
     )
-    return `www.${postID}.mp4`
+    const videoUrl = await page.evaluate(
+      async ({ apiURL }) => {
+        // eslint-disable-next-line no-undef
+        const response = await fetch(apiURL)
+        const data = await response.json()
+        const videoUrl = data.data['shortcode_media']['video_url']
+        console.log(`[BOT EXTRACT_DATA] ${videoUrl}`)
+        return videoUrl
+      },
+      { apiURL }
+    )
+
+    return videoUrl
   }
 
+  const dataFromVideos = await page.evaluate(
+    async ({ maxPostGet }) => {
+      const exports = []
+      // eslint-disable-next-line no-undef
+      const elements = [...document.querySelectorAll('article a')]
+      const postsID = []
+
+      for (let index in elements.slice(0, maxPostGet || elements.length)) {
+        const formatado = elements[index]
+          .getAttribute('href')
+          .replace('/p/', '')
+          .replace('/', '')
+        postsID.push(formatado)
+      }
+
+      console.log({ postsID })
+      for (let index in postsID) {
+        const postID = postsID[index]
+        const videoUrl = await getLinkForDownload(postID)
+
+        if (videoUrl) exports.push({ postID, videoUrl })
+      }
+      return exports
+    },
+    { maxPostGet }
+  )
+  console.log(`[BOT EXTRACT_DATA] ${JSON.stringify(dataFromVideos)}`)
   const data = await dataFromVideos
   await addData('postsTodo', data)
 }
