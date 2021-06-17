8086b0cd887c13b47563ed0c843ca25a90202f21
caio <kayoroxa@gmai.com>
2021-06-17 11:02:56 -0300

    index with all parameters and instagrams list

--- newVersion/index.js
+++ newVersion/index.js
@@ -1,12 +1,22 @@
 const ZeusInsta = require('./zeusInsta')
 
+const instagrams = [
+  'https://www.instagram.com/videos_engracados2k/',
+  'https://www.instagram.com/videospradarrisada/',
+  'https://www.instagram.com/memes24horasoficial/',
+  'https://www.instagram.com/rindo.ate.2091/',
+]
+
 async function main() {
   const zeus = await ZeusInsta({
     downloadFolder: 'videos_baixados',
     readyVideosFolder: 'videos_prontos',
     dataBaseFolder: './dataBaseFolder',
+    maxPostGet: 1,
+    instagrams,
+    headless: true,
+    // startBrowser: false,
   })
-  // await zeus.startBrowser()
 
   await zeus.extractDataFromVideos()
   await zeus.downloadVideos()
@@ -14,8 +24,9 @@ async function main() {
 
   await zeus.closeBrowser()
 }
+
 try {
   main()
 } catch (error) {
-  console.log(`error: ${error}`)
+  console.log(`error: ${error.message}`)
 }
